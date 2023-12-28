842802fdca991d4363060a7274f898211b838499
JW <mgcmasterchief@gmail.com>
2023-12-28 17:47:44 +0200

    feat: creating login form;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import homeLogo from '/public/imgs/cyberpunk-logo.png';
 import Image from 'next/image';
+
+import LoginForm from '@/components/loginForm/loginForm';
 
 export default function HomePage() {
   return (
-    <div className="grid grid-cols-2">
-      <div className=" bg-yellow-300"></div>
-      <div></div>
+    <div className="bg-home-bg bg-no-repeat bg-cover grid grid-cols-[60%_40%]  w-screen h-screen m-auto">
+      <div className="w-full h-full relative inset-0 box-shadow-inner">
+        <Image
+          className="w-[70%] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 absolute object-contain drop-shadow-[0px_10px_10px_#6a6c03]"
+          src={homeLogo}
+          alt="Cyberpunk 2077"
+          priority
+        />
+      </div>
+      <div className="bg-zinc-800 box-shadow-left flex items-center justify-center">
+        <LoginForm />
+      </div>
     </div>
   );
 }
---
+++ src/components/loginForm/loginForm.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useFormState } from 'react-dom';
+
+export default function LoginForm() {
+  return (
+    <div className="text-white">
+      <div className="text-center">
+        <h3 className=" text-2xl m-2 font-light italic text-cyan-400">
+          Welcome to
+        </h3>
+        <h1 className="text-5xl mb-5 font-bold uppercase tracking-wider text-[yellow] ">
+          CyberNet
+        </h1>
+      </div>
+    </div>
+  );
+}
